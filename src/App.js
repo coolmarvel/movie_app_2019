25f3e1755fa7823913b72088771420fadc552c9e
coolmarvel <marvel97@naver.com>
2021-10-22 17:31:10 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
+import Movie from "./Movie";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} />
-    </div>
-  )
-}
-
-const foodILike = [
-  {
-    name: "kimchi",
-    image: "https://health.chosun.com/site/data/img_dir/2021/09/01/2021090100998_0.jpg",
-  },
-  {
-    name: "ramen",
-    image: "https://w.namu.la/s/9f15f198aab1b14c8aa47e96a91a9d03331ecb7b5b892c803159d39b0d77ab4be30e2f15f66191284d7dad8371989329cc1c80810745e980a6949ae5e3589df64d239a3ca63c5a7574a4ad2656633c8c",
-  },
-  {
-    name: "samgyeopsal",
-    image: "https://post-phinf.pstatic.net/MjAyMDAzMDNfMTcg/MDAxNTgzMTkwNjA3ODQ5.kUXPHqGJ2xPDSu_3FiEoFC3kY9QyQ_g9CziCGrFSDuEg.LpCfOTbc5qth9d-GKzGv9jwj2VKhcqmPHp5cp1KJYEsg.JPEG/IM_food02.jpg?type=w1200",
-  },
-  {
-    name: "chukumi",
-    image: "https://static.wtable.co.kr/image/production/service/recipe/707/d403b168-c865-4b5e-9afb-58c48e5feaec.jpg",
-  },
-  {
-    name: "gimbap",
-    image: "https://static.wtable.co.kr/image-resize/production/service/recipe/689/4x3/edd1e01f-4502-453b-8b61-688941aefb76.jpg",
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getmovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getmovies();
+  };
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading..." : movies.map(movie => {
+          return (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            />
+          )
+        })}
+      </div>
+    );
+  };
 }
 
 export default App;
